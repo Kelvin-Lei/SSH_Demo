3a091e2d738bb2ae8f0ab397df9ed380ddc7a649
Klei <leiyun01@gmail.com>
2015-12-19 10:35:11 +0800

    使用查询缓存
    1、开启Hibernate二级缓存 -> 开启查询缓存
    2、通过SessionFactory管理查询缓存

--- src/com/kelvin/test/TestSecondCache.java
+++ src/com/kelvin/test/TestSecondCache.java
@@ -1,5 +1,8 @@
 package com.kelvin.test;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.junit.Test;
 
@@ -13,6 +16,8 @@ public class TestSecondCache {
 	 */
 	@Test
 	public void test1() {
+		System.out.println("=============二级缓存=============");
+		
 		Session session1 = HibernateUtil.getSession();
 		Emp e1 = (Emp)session1.get(Emp.class, 1);
 		System.out.println(e1.getName());
@@ -28,5 +33,39 @@ public class TestSecondCache {
 		
 		session1.close();
 		session2.close();
+	}
+	
+	/**
+	 * 查询缓存
+	 */
+	@Test
+	public void test2() {
+		System.out.println("=============查询缓存=============");
+		
+		Session session = HibernateUtil.getSession();
+		
+		String hql = "from Emp";
+		Query query = session.createQuery(hql);
+		// 开启查询缓存
+		query.setCacheable(true);
+		List<Emp> emps = query.list();
+		for(Emp emp : emps) {
+			System.out.println(emp.getId() + " " + emp.getName());
+		}
+		System.out.println("------------------------");
+		
+		// 通过SessionFactory对二级缓存进行管理
+		HibernateUtil.getSessionFactory().evictQueries();
+		
+		hql = "from Emp";
+		query = session.createQuery(hql);
+		// 开启查询缓存
+		query.setCacheable(true);
+		emps = query.list();
+		for(Emp emp : emps) {
+			System.out.println(emp.getId() + " " + emp.getName());
+		}
+		
+		session.close();
 	}
 }
